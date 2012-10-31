0379dbd9744e52dee102975f19dc694f71657df1
Jerad Bitner <sirkitree@gmail.com>
2012-10-31 14:34:21 -0600

    Initial commit of posterboy

---
+++ public/js/controllers.js
@@ -0,0 +1,87 @@
+'use strict';
+
+/* Controllers */
+
+function IndexCtrl($scope, $http, $location) {
+  $http.get('/api/posts').
+    success(function(data, status, headers, config) {
+      $scope.posts = data.posts.reverse();
+    });
+
+  $scope.form = {};
+  $scope.submitPost = function() {
+    $http.post('/api/post', $scope.form).
+      success(function(data) {
+        $location.path('/');
+        $http.get('/api/posts').
+          success(function(data, status, headers, config) {
+            $scope.posts = data.posts.reverse();
+          });
+      });
+  };
+
+  // @todo: Expand the textarea when the size is too small.
+  $scope.expand = function() {
+    console.log($scope);
+    var textarea = $scope.form.text;
+    console.log(textarea.length);
+    // if (textarea.length > 5){
+    //   textarea.cols = 50;
+    //   textarea.rows = 50;
+    // }  else{
+    //    textarea.cols = 10;
+    //    textarea.rows = 15;
+    // }
+  }
+
+}
+
+function AddPostCtrl($scope, $http, $location) {
+  $scope.form = {};
+  $scope.submitPost = function() {
+    $http.post('/api/post', $scope.form).
+      success(function(data) {
+        $location.path('/');
+      });
+  };
+}
+
+function ReadPostCtrl($scope, $http, $routeParams) {
+  $http.get('/api/post/' + $routeParams.id).
+    success(function(data) {
+      $scope.post = data.post;
+    });
+}
+
+function EditPostCtrl($scope, $http, $location, $routeParams) {
+  $scope.form = {};
+  $http.get('/api/post/' + $routeParams.id).
+    success(function(data) {
+      $scope.form = data.post;
+    });
+
+  $scope.editPost = function() {
+    $http.put('/api/post/' + $routeParams.id, $scope.form).
+      success(function(data) {
+        $location.url('/readPost/' + $routeParams.id);
+      });
+  };
+}
+
+function DeletePostCtrl($scope, $http, $location, $routeParams) {
+  $http.get('/api/post/' + $routeParams.id).
+    success(function(data) {
+      $scope.post = data.post;
+    });
+
+  $scope.deletePost = function() {
+    $http.delete('/api/post/' + $routeParams.id).
+      success(function(data) {
+        $location.url('/');
+      });
+  };
+
+  $scope.home = function() {
+    $location.url('/');
+  };
+}
