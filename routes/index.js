cb1e187c9fc5830d46ee782380b2ff7438d3f3d8
Jerad Bitner <sirkitree@gmail.com>
2012-11-17 19:25:33 -0700

    Initial use of captchagen to display a captcha png.

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,14 @@ exports.index = function(req, res){
   res.render('index');
 };
 
-exports.partials = function (req, res) {
+exports.partials = function(req, res) {
   var name = req.params.name;
   res.render('partials/' + name);
+};
+
+exports.captcha = function(req, res) {
+  var c = require('captchagen'),
+  captcha = c.generate();
+  res.type('image/png');
+  res.end(captcha.buffer());
 };
